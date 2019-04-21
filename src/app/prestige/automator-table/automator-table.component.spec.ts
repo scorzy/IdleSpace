9dd9459f4f46d58d15aefe6709123860461ebdb1
scorzy <scorzy@users.noreply.github.com>
2019-04-21 23:45:55 +0200

    test

--- src/app/prestige/automator-table/automator-table.component.spec.ts
+++ src/app/prestige/automator-table/automator-table.component.spec.ts
@@ -1,25 +1,38 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed } from "@angular/core/testing";
 
-import { AutomatorTableComponent } from './automator-table.component';
+import { AutomatorTableComponent } from "./automator-table.component";
+import { CUSTOM_ELEMENTS_SCHEMA } from "@angular/core";
+import { defaultImport } from "src/app/app.component.spec";
+import { ActionHeaderComponent } from "src/app/action/action-header/action-header.component";
+import { FormatPipe } from "src/app/format.pipe";
+import { MainService } from "src/app/main.service";
+import { OptionsService } from "src/app/options.service";
 
-describe('AutomatorTableComponent', () => {
+describe("AutomatorTableComponent", () => {
   let component: AutomatorTableComponent;
   let fixture: ComponentFixture<AutomatorTableComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ AutomatorTableComponent ]
-    })
-    .compileComponents();
+      schemas: [CUSTOM_ELEMENTS_SCHEMA],
+      imports: defaultImport(),
+      declarations: [
+        AutomatorTableComponent,
+        ActionHeaderComponent,
+        FormatPipe
+      ],
+      providers: [MainService, OptionsService]
+    }).compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(AutomatorTableComponent);
     component = fixture.componentInstance;
+    component.ms.start();
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it("should create", () => {
     expect(component).toBeTruthy();
   });
 });
