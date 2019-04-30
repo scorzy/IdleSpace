2e5d09c695c97bcf73059f4c975245b999a08630
scorzy <scorzy@users.noreply.github.com>
2019-04-30 21:58:15 +0200

    descriptions

--- src/app/model/descriptions.ts
+++ src/app/model/descriptions.ts
@@ -14,24 +14,39 @@ export const descriptions = {
       "Computing",
       "Computing is processing power. It cannot be stored but instead, if unused, is used for researching."
     ],
-    b: ["Battery", "Battery"],
+    b: ["Battery", "Batteries increase your maximum energy storage."],
     i: ["Missile", "Interplanetary missiles destroy enemy defense."],
     m1: ["Mining Drone", "Metal Mining Drone, yields metal."],
     c1: ["Crystal Drone", "Crystal Mining Drone, yields crystal."],
     a1: ["Foundry Drone", "Foundry Drone convert metal and crystal to alloy."],
     e1: ["Technician", "Technicians yields energy."],
-    hs: ["Habitable Space", "Space for buildings."],
-    md: ["Mining District", ""],
-    cd: ["Crystal District", ""],
+    hs: [
+      "Habitable Space",
+      "Space for buildings. You can obtain more Habitable Space from battle."
+    ],
+    md: [
+      "Mining District",
+      "Space for metal mines. You can obtain more Mining District from battle."
+    ],
+    cd: [
+      "Crystal District",
+      "Space for crystal mines. You can obtain more Crystal District from battle."
+    ],
     f1: ["Computing Unit", ""],
     N: ["Naval Capacity", ""],
     S1: ["Worker Drone", "Worker Drone makes ships and ships upgrades."],
-    SP: ["Shipyard Prog.", "This represent work done by your Shipyard"],
+    SP: [
+      "Shipyard Prog.",
+      "This represent work done by your Shipyard. It's available only when you make or upgrade ships."
+    ],
     X1: [
       "Searching Drone",
       "Searching Drone allow you to search for new enemies."
     ],
-    XP: ["Searching Prog.", ""],
+    XP: [
+      "Searching Prog.",
+      "This represent work done by your Searching Drone. It's available only when you are searching for new targets."
+    ],
     W1: [
       "Warrior Drone",
       "Warrior Drone increase your maximum naval capacity, allowing you to make more ships."
