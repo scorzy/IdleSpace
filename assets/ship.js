94d880ff028f3e045ff2bbd91ecbc329f6f5a187
scorzy <scorzy@users.noreply.github.com>
2019-06-13 21:06:43 +0200

    Auto-generated commit

--- assets/ship.js
+++ assets/ship.js
@@ -1,17 +1,25 @@
 var Ship = /** @class */ (function () {
     function Ship() {
+        this.armor = new Decimal();
+        this.shield = new Decimal();
         this.modules = new Array();
     }
     Ship.prototype.getCopy = function () {
-        var ret = new Ship();
+        var ret = Ship.Ships.pop() || new Ship();
         ret.id = this.id;
-        ret.armor = new Decimal(this.armor);
-        ret.originalArmor = new Decimal(this.armor);
-        ret.shield = new Decimal(this.shield);
-        ret.originalShield = new Decimal(this.shield);
+        ret.armor.fromDecimal(this.armor);
+        ret.shield.fromDecimal(this.shield);
+        // Original value shouldn't get modified, reuse original object instead of creating 10000+ new ones for max ships.
+        ret.originalArmor = this.originalArmor;
+        ret.originalShield = this.originalShield;
         ret.modules = this.modules;
         ret.explosionLevel = this.explosionLevel;
         return ret;
     };
+    Ship.prototype.free = function () {
+        // Reuse object to prevent gc
+        Ship.Ships.push(this);
+    };
+    Ship.Ships = new Array();
     return Ship;
 }());
