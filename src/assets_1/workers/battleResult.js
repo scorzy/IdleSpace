4426ffc68b154941045df021617030e433d35cb5
scorzy <scorzy@users.noreply.github.com>
2019-08-05 22:13:25 +0200

    test and gitignore

--- src/assets_1/workers/battleResult.js
+++
@@ -1,9 +0,0 @@
-// tslint:disable-next-line: no-unused-declaration
-var BattleResult = /** @class */ (function() {
-  function BattleResult() {
-    this.result = "";
-    this.playerLost = new Array();
-    this.enemyLost = new Array();
-  }
-  return BattleResult;
-})();
