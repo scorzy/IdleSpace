25e15c6de150078618e42e8256658c0203ee75ec
scorzy <scorzy@users.noreply.github.com>
2019-08-19 21:51:06 +0200

    Auto-generated commit

--- assets/compressRequest2.js
+++ assets/compressRequest2.js
@@ -1,10 +1,1 @@
-// tslint:disable-next-line: no-unused-declaration
-var CompressRequest2 = /** @class */ (function () {
-    function CompressRequest2(obj, zipped, compress, requestId) {
-        this.obj = obj;
-        this.zipped = zipped;
-        this.compress = compress;
-        this.requestId = requestId;
-    }
-    return CompressRequest2;
-}());
+var CompressRequest2=function(obj,zipped,compress,requestId){this.obj=obj,this.zipped=zipped,this.compress=compress,this.requestId=requestId};
